fc02da40eadbda2fd715a29275a2a7a56884c511
mega5800 <43865655+mega5800@users.noreply.github.com>
2020-06-24 12:38:02 +0300

    Add files via upload

---
+++ C Sharp Exercise 4/Ex04.Menus.Test/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Ex04.Menus.Test
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            runExercise4();
+        }
+
+        private static void runExercise4()
+        {
+            InterfaceMenu interfaceMenu = new InterfaceMenu();
+            DelegateMenu delegateMenu = new DelegateMenu();
+
+            interfaceMenu.RunInterfaceMenu();
+            delegateMenu.RunDelegateMenu();
+        }
+    }
+}
