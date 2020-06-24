d6743af24cbc3a0fda2aa321bf6fdf3644934b4b
mega5800 <43865655+mega5800@users.noreply.github.com>
2020-06-24 12:10:07 +0300

    Add files via upload

---
+++ C Sharp Exercise 2/B20_Ex02/Player.cs
@@ -0,0 +1,49 @@
+﻿namespace B20_Ex02
+{
+    public class Player
+    {
+        // MEMBER VARIABLES
+        private readonly bool r_IsHuman;
+        private readonly string r_PlayerName;
+        private int m_Score;
+        private PlayerStep m_FirstStep;
+        private PlayerStep m_SecondStep;
+
+        // CTOR
+        public Player(string i_PlayerName, bool i_IsHuman)
+        {
+            this.r_PlayerName = i_PlayerName;
+            this.r_IsHuman = i_IsHuman;
+            this.m_Score = 0;
+        }
+
+        // PROPERTIES
+        public int Score
+        {
+            get { return this.m_Score; }
+            set { this.m_Score = value; }
+        }
+
+        public PlayerStep FirstStep
+        {
+            get { return this.m_FirstStep; }
+            set { this.m_FirstStep = value; }
+        }
+
+        public PlayerStep SecondStep
+        {
+            get { return this.m_SecondStep; }
+            set { this.m_SecondStep = value; }
+        }
+
+        public string PlayerName
+        {
+            get { return this.r_PlayerName; }
+        }
+
+        public bool IsHuman
+        {
+            get { return this.r_IsHuman; }
+        }
+    }
+}
---
+++ C Sharp Exercise 2/B20_Ex02/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace B20_Ex02
+{
+    public class Program
+    {
+        // MAIN METHOD
+        public static void Main()
+        {
+            runExercise2();
+        }
+
+        // # RUN METHOD # 
+        private static void runExercise2()
+        {
+            string playerOneName = string.Empty, playerTwoName = string.Empty;
+            GameUIComponent gameUIComponent = null;
+            Player playerOne = null, playerTwo = null;
+            int boardHeight, boardWidth;
+            bool isHuman, isGameActive = true;
+
+            playerOneName = GameUIComponent.GetValidPlayerName("first");
+            playerOne = new Player(playerOneName, true);
+            playerTwoName = GameUIComponent.ChooseOpponent(out isHuman);
+            playerTwo = new Player(playerTwoName, isHuman);
+            GameUIComponent.GreetPlayers(playerOne, playerTwo);
+            gameUIComponent = new GameUIComponent();
+            while (isGameActive)
+            {
+                GameUIComponent.GetValidBoardDimensions(out boardHeight, out boardWidth);
+                gameUIComponent.CreateLogicComponent(boardHeight, boardWidth, playerOne, playerTwo);
+                gameUIComponent.StartMatch();
+                gameUIComponent.AnnounceWinner();
+                isGameActive = gameUIComponent.NewGameQuestion();
+            }
+        }
+    }
+}
